bb1a66c588df90080bae72e477e2f2347d18ff6e
Bogdan Preda <bogdan.preda@themeisle.com>
2017-01-23 18:31:46 +0200

    changed to use compare

--- grunt/http.js
+++ grunt/http.js
@@ -42,7 +42,7 @@ module.exports = {
                 console.log(body)
             },
             form: {
-                mode: 'latest',
+                mode: 'compare',
                 slug: '<%= package.wraithSlug %>'
             }
         }
