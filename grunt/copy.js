cddb7c48390d19cce7384ff158530c9a9c2d2d77
Marius Cristea <marius.cristea@vertistudio.com>
2017-08-05 13:30:37 +0300

    Fixed lite to follow the distignore pattern.

--- grunt/copy.js
+++ grunt/copy.js
@@ -3,35 +3,52 @@
  */
 
 module.exports = function (grunt, options) {
-	return {
-		full: {
-			files: [{
-				src: ['**'],
-				dest: 'dist/<%= package.name %>/',
-				filter: function (filepath) {
-					var exclude = grunt.file.read('.distignore');
-					var cwd = grunt.file.read('.distignore').split(/\r|\n/);
-					cwd = cwd.filter(function (e) {
-						return e
-					});
-					for (index in cwd) {
-						var file = cwd[index];
-						if (filepath.indexOf(file) === 0) {
-							return false;
-						}
-					}
-					return true;
-				}
-			}]
-		},
-		lite: {
-			files: [{
-				src: ['**'],
-				expand: true,
-				cwd: '../<%= package.litename %>/',
-				dest: '../<%= package.name %>/dist/<%= package.litename %>/'
-			}
-			]
-		}
-	};
+    return {
+        full: {
+            files: [{
+                src: ['**'],
+                dest: 'dist/<%= package.name %>/',
+                filter: function (filepath) {
+                    var exclude = grunt.file.read('.distignore');
+                    var cwd = grunt.file.read('.distignore').split(/\r|\n/);
+                    cwd = cwd.filter(function (e) {
+                        return e
+                    });
+                    for (index in cwd) {
+                        var file = cwd[index];
+                        if (filepath.indexOf(file) === 0) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }]
+        },
+        lite: {
+            files: [{
+                src: ['**'],
+                expand: true,
+                cwd: '../<%= package.litename %>/',
+                dest: '../<%= package.name %>/dist/<%= package.litename %>/',
+                filter: function (filepath) {
+                    filepath = filepath.replace(this.cwd,'');
+
+                    var exclude = grunt.file.read('.distignore');
+
+                    var cwd = grunt.file.read('.distignore').split(/\r|\n/);
+                    cwd = cwd.filter(function (e) {
+                        return e
+                    });
+                    for (index in cwd) {
+                        var file = cwd[index];
+                        if (filepath.indexOf(file) === 0) {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            ]
+        }
+    };
 };
