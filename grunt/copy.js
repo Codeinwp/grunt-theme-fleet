6394a97393adf5eed718db0912c3c38a1054a38d
selu91 <cristea.marius.alexandru@gmail.com>
2017-08-08 17:24:46 +0300

    Fixed lite deploy.

--- grunt/copy.js
+++ grunt/copy.js
@@ -3,52 +3,60 @@
  */
 
 module.exports = function (grunt, options) {
-    return {
-        full: {
-            files: [{
-                src: ['**'],
-                dest: 'dist/<%= package.name %>/',
-                filter: function (filepath) {
-                    var exclude = grunt.file.read('.distignore');
-                    var cwd = grunt.file.read('.distignore').split(/\r|\n/);
-                    cwd = cwd.filter(function (e) {
-                        return e
-                    });
-                    for (index in cwd) {
-                        var file = cwd[index];
-                        if (filepath.indexOf(file) === 0) {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-            }]
-        },
-        lite: {
-            files: [{
-                src: ['**'],
-                expand: true,
-                cwd: '../<%= package.litename %>/',
-                dest: '../<%= package.name %>/dist/<%= package.litename %>/',
-                filter: function (filepath) {
-                    filepath = filepath.replace(this.cwd,'');
+	return {
+		full: {
+			files: [{
+				src: ['**'],
+				dest: 'dist/<%= package.name %>/',
+				filter: function (filepath) {
+					var exclude = grunt.file.read('.distignore');
+					var cwd = grunt.file.read('.distignore').split(/\r|\n/);
+					cwd = cwd.filter(function (e) {
+						return e
+					});
+					for (index in cwd) {
+						var file = cwd[index];
+						if (filepath.indexOf(file) === 0) {
+							return false;
+						}
+					}
+					return true;
+				}
+			}]
+		},
+		lite: {
+			files: [{
+				src: ['**'],
+				expand: true,
+				cwd: '../<%= package.litename %>/',
+				dest: '../<%= package.name %>/dist/<%= package.litename %>/',
+				filter: function (filepath) {
+					var unixifyPath = function (filepath) {
 
-                    var exclude = grunt.file.read('.distignore');
+						var isWindows = process.platform === 'win32';
+						if (isWindows) {
+							return filepath.replace(/\\/g, '/');
+						} else {
+							return filepath;
+						}
+					};
+					filepath = unixifyPath(filepath).replace(unixifyPath(this.cwd), '');
+					var exclude = grunt.file.read('.distignore');
 
-                    var cwd = grunt.file.read('.distignore').split(/\r|\n/);
-                    cwd = cwd.filter(function (e) {
-                        return e
-                    });
-                    for (index in cwd) {
-                        var file = cwd[index];
-                        if (filepath.indexOf(file) === 0) {
-                            return false;
-                        }
-                    }
-                    return true;
-                }
-            }
-            ]
-        }
-    };
+					var cwd = grunt.file.read('.distignore').split(/\r|\n/);
+					cwd = cwd.filter(function (e) {
+						return e
+					});
+					for (index in cwd) {
+						var file = cwd[index];
+						if (filepath.indexOf(file) === 0) {
+							return false;
+						}
+					}
+					return true;
+				}
+			}
+			]
+		}
+	};
 };
